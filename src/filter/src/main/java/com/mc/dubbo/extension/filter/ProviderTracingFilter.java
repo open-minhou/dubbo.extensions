b630eef39db13baa3b3fd819f8d913896a0fd5f5
MingHao <MingHao@mallcoo.cn>
2018-02-08 18:22:29 +0800

    update

---
+++ src/filter/src/main/java/com/mc/dubbo/extension/filter/ProviderTracingFilter.java
@@ -0,0 +1,10 @@
+package com.mc.dubbo.extension.filter;
+
+import com.alibaba.dubbo.rpc.protocol.dubbo.filter.TraceFilter;
+
+/**
+ * @author minghao
+ * @since  2018/1/30 14:48
+ */
+public class ProviderTracingFilter extends TraceFilter {
+}
